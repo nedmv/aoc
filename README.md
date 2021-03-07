0e31ad44147ba999ce2a7cd82492d4461ed6b533
nedmv <nedmvl@rambler.ru>
2021-03-07 22:15:38 +0300

    feat: add day 5

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Overview
 
-This repository contains my solutions for [Advent of Code](https://adventofcode.com/) puzzles.
+This repository contains my solutions for [Advent of Code](https://adventofcode.com/2020/) puzzles.
 
 However, the main purpose is NOT solving these puzzles, but practicing programming and playing with some shiny things in the process.
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <regex>
 #include <string>
 #include <vector>
+#include <bitset> //day 5
 
 #include "Task.hpp"
 
@@ -226,6 +227,46 @@ const string solve_04b(vector<string> input) {
   return to_string(valid_passports);
 }
 
+//
+// DAY 5
+// 
+
+ulong count_seat_id(string input) {
+  string row = input.substr(0, 7);
+  string col = input.substr(7, 3);
+  replace(row.begin(), row.end(), 'F', '0');
+  replace(row.begin(), row.end(), 'B', '1');
+  replace(col.begin(), col.end(), 'L', '0');
+  replace(col.begin(), col.end(), 'R', '1');
+  return bitset<8>(row).to_ulong() * 8 + bitset<8>(col).to_ulong();
+}
+
+const string solve_05a(vector<string> input) {
+  ulong max_id = 0;
+  ulong current_id = 0;
+  for (size_t i = 0; i<input.size(); i++) {
+    current_id = count_seat_id(input[i]);
+    if (current_id > max_id) max_id = current_id;
+  }
+  return to_string(max_id);
+}
+
+const string solve_05b(vector<string> input) {
+  ulong current_id = 0;
+  //could use 05a output, but independence is more valuable 
+  const uint16_t max_id = 128*8 - 1; 
+  ulong min_id = max_id;
+  bitset<max_id> b;
+  b.set();
+  for (size_t i = 0; i<input.size(); i++) {
+    current_id = count_seat_id(input[i]);
+    b[current_id] = false;
+    if (current_id < min_id) min_id = current_id;
+  }
+  b = b >> min_id;
+  return to_string(b._Find_first() + min_id);
+}
+
 int main() {
   Task t = Task();
   t.execute("01a", solve_01a);
@@ -240,5 +281,7 @@ int main() {
   t.execute("04a", solve_04a);
   t.execute("04b", solve_04b);
 
+  t.execute("05a", solve_05a);
+  t.execute("05b", solve_05b);
   return 0;
 }
