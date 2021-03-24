990b6b56df5e8ce72520f376c110f4c0b8f10fb0
nedmv <nedmvl@rambler.ru>
2021-03-24 23:29:37 +0300

    feat(2020): add 16b

--- 2020/16/16.cpp
+++ 2020/16/16.cpp
@@ -5,8 +5,9 @@ class Field {
  public:
   Field(string &input) {
     smatch match;
-    if (regex_match(input, match,
-                regex(R"(([^:]+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+))"))) {
+    if (regex_match(
+            input, match,
+            regex(R"(([^:]+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+))"))) {
       _name = match[1].str();
       _min1 = stoi(match[2].str());
       _max1 = stoi(match[3].str());
@@ -39,6 +40,35 @@ vector<bool> checkRanges(uint32_t x, vector<Field> &fields) {
   return result;
 }
 
+bool checkRanges(vector<uint32_t> &values, vector<Field> &fields, vector<vector<bool>> &checks) {
+  size_t size = fields.size();
+  size_t counter;
+  uint32_t value;
+  for (size_t i = 0; i < size; ++i) {
+    value = values[i];
+    counter = 0;
+    for (size_t j = 0; j < size; ++j) {
+      if (fields[j].inRange(value)) {
+        checks[i][j] = true;
+        counter++;
+      }
+    }
+    if (!counter) return false;
+  }
+  return true;
+}
+
+void mergeChecks(vector<vector<bool>> &in, vector<vector<bool>> &merged) {
+  size_t size = in.size();
+  for (size_t i = 0; i < size; ++i) {
+    for (size_t j = 0; j < size; ++j) {
+      if (in[i][j] != merged[i][j]) {
+        merged[i][j] = false;
+      }
+    }
+  }
+}
+
 bool inRanges(uint32_t x, vector<Field> &fields) {
   for (bool check : checkRanges(x, fields)) {
     if (check) return true;
@@ -57,7 +87,7 @@ const std::string y2020::solve_16a(std::vector<std::string> input) {
 
   while (input[i] != "nearby tickets:") {
     i++;
-  } 
+  }
   i++;
 
   string s;
@@ -74,12 +104,12 @@ const std::string y2020::solve_16a(std::vector<std::string> input) {
       num = stoi(s.substr(begin, end - begin));
       cout << num << " ";
       if (!inRanges(num, fields)) {
-        sum+=num;
+        sum += num;
       }
       if (end == s.npos) {
         cout << endl;
         break;
-      }  
+      }
       begin = end + 1;
       end = s.find(",", begin);
     }
@@ -87,6 +117,76 @@ const std::string y2020::solve_16a(std::vector<std::string> input) {
   return to_string(sum);
 }
 
+class Ticket {
+ public:
+  Ticket(string s) {
+    size_t begin = 0;
+    size_t end = s.find(",", begin);
+    while (true) {
+      _values.push_back(stoi(s.substr(begin, end - begin)));
+      if (end == s.npos) {
+        break;
+      }
+      begin = end + 1;
+      end = s.find(",", begin);
+    }
+  }
+  bool isValid(vector<Field> &fields) {
+    for (auto &value : _values) {
+      if (!inRanges(value, fields)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+ private:
+  vector<uint32_t> _values;
+};
+
+vector<uint32_t> loadTicket(string &s) {
+  vector<uint32_t> result;
+  size_t begin = 0;
+  size_t end = s.find(",", begin);
+  while (true) {
+    result.push_back(stoi(s.substr(begin, end - begin)));
+    if (end == s.npos) {
+      break;
+    }
+    begin = end + 1;
+    end = s.find(",", begin);
+  }
+  return result;
+}
+
 const std::string y2020::solve_16b(std::vector<std::string> input) {
+  vector<Field> fields;
+  size_t i = 0;
+
+  while (!input[i].empty()) {
+    fields.push_back(Field(input[i]));
+    i++;
+  }
+
+  i += 2;
+
+  auto myTicket = loadTicket(input[i]);
+  size_t size = myTicket.size();
+
+  i += 3;
+
+  vector<uint32_t> ticket;
+
+  vector<vector<bool>> possibleChecks(size, vector<bool>(size, true));
+  vector<vector<bool>> currentChecks(size, vector<bool>(size, false));
+
+  for (i; i < input.size(); i++) {
+    ticket = loadTicket(input[i]);
+    if (checkRanges(ticket, fields, currentChecks)) {
+
+    }
+
+  }
+
   return ERROR_STRING;
 }
