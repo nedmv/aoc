89ef49c81882d75110a13b01b1cecd91f06f1a85
nedmv <nedmvl@rambler.ru>
2021-12-18 16:44:14 +0300

    feat(2021): add day 18

---
+++ src/2021/18/18.cpp
@@ -0,0 +1,135 @@
+// Puzzle is available at https://adventofcode.com/2021/day/18
+// Keywords:
+#include "../2021.hpp"
+#include "../../utils.hpp"
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+struct RegularNumber {
+  RegularNumber(int value, int level): value{value}, level{level} {}
+  ~RegularNumber() {}
+  int value;
+  int level;
+};
+
+static vector<RegularNumber> parseSnailfishNumber(const string &str) {
+  vector<RegularNumber> result;
+  int openedBraces = 0;
+
+  for (size_t i = 0; i < str.size(); i++) {
+    if (('0' <= str[i]) && (str[i] <= '9')) {
+      result.push_back(RegularNumber(str[i] - '0', openedBraces));
+    } else if (str[i] == '[') {
+        openedBraces++;
+    } else if (str[i] == ']') {
+      openedBraces--;
+    }
+  }
+  return result;
+}
+
+static bool explode(vector<RegularNumber> &num) {
+  bool exploded = false;
+  for (auto it = num.begin(); it != num.end(); ++it) {
+    if (it->level == 5) {
+      if (it != num.begin()) {
+        (it-1)->value += it->value;
+      }
+      if ((it + 2) != num.end()) {
+        (it+2)->value += (it+1)->value;
+      }
+      *it = RegularNumber(0, 4);
+      num.erase(it+1);
+      exploded = true;
+      break;
+    }
+  }
+  return exploded;
+}
+
+static bool split(vector<RegularNumber> &num) {
+  bool splitted = false;
+  for (auto it = num.begin(); it != num.end(); ++it) {
+    if (it->value >= 10) {
+      int value = it->value;
+      it->value = value/2;
+      it->level++;
+      num.insert(it+1, RegularNumber(it->value + value%2, it->level));
+      splitted = true;
+      break;
+    }
+  }
+  return splitted;
+}
+
+static void reduce(vector<RegularNumber> &num) {
+  while(true) {
+    if (explode(num) || split(num)) {
+      continue;
+    } else {
+      break;
+    }
+  }
+}
+
+static vector<RegularNumber> sum(vector<RegularNumber> &num1, vector<RegularNumber> &num2) {
+  vector result = num1;
+  for (auto &num: num2) {
+    result.push_back(num);
+  }
+  for (int i = 0; i < result.size(); i++) {
+    result[i].level++;
+  }
+  reduce(result);
+  return result;
+}
+
+static size_t countMagnitude(vector<RegularNumber> num) {
+  for (int level = 4; level > 0; level--) {
+    auto hasLevel = [level](RegularNumber reg) {
+      return reg.level == level;
+    };
+    while (true) {
+      auto it = find_if(num.begin(), num.end(), hasLevel);
+      if (it != num.end()) {
+        it->level--;
+        it->value = it->value * 3 + (it+1)->value * 2;
+        num.erase(it+1);
+      } else {
+        break;
+      }
+    }
+  }
+  return num[0].value;
+}
+
+const std::string y2021::solve_18a(std::vector<std::string> input) {
+  auto result = parseSnailfishNumber(input[0]);
+  for (int i = 1; i < input.size(); i++) {
+    auto current = parseSnailfishNumber(input[i]);
+    result = sum(result, current);
+  }
+  return to_string(countMagnitude(result));
+}
+
+const std::string y2021::solve_18b(std::vector<std::string> input) {
+  size_t size = input.size();
+  size_t maxMagnitude = 0;
+  vector<vector<RegularNumber>> nums;
+  for (int i = 0; i < size; i++) {
+    nums.push_back(parseSnailfishNumber(input[i]));
+  }
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (i != j) {
+        size_t magnitude = countMagnitude(sum(nums[i], nums[j]));
+        if (magnitude > maxMagnitude) {
+          maxMagnitude = magnitude;
+        }
+      }
+    }
+  }
+  return to_string(maxMagnitude);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -18,56 +18,59 @@ void year2021() {
   Task t = Task();
   t.setYear(2021);
 
-  t.execute("01a", solve_01a);
-  t.execute("01b", solve_01b);
+  // t.execute("01a", solve_01a);
+  // t.execute("01b", solve_01b);
 
-  t.execute("02a", solve_02a);
-  t.execute("02b", solve_02b);
+  // t.execute("02a", solve_02a);
+  // t.execute("02b", solve_02b);
 
-  t.execute("03a", solve_03a);
-  t.execute("03b", solve_03b);
+  // t.execute("03a", solve_03a);
+  // t.execute("03b", solve_03b);
 
-  t.execute("04a", solve_04a);
-  t.execute("04b", solve_04b);
+  // t.execute("04a", solve_04a);
+  // t.execute("04b", solve_04b);
 
-  t.execute("05a", solve_05a);
-  t.execute("05b", solve_05b);
+  // t.execute("05a", solve_05a);
+  // t.execute("05b", solve_05b);
 
-  t.execute("06a", solve_06a);
-  t.execute("06b", solve_06b);
+  // t.execute("06a", solve_06a);
+  // t.execute("06b", solve_06b);
 
-  t.execute("07a", solve_07a);
-  t.execute("07b", solve_07b);
+  // t.execute("07a", solve_07a);
+  // t.execute("07b", solve_07b);
 
-  t.execute("08a", solve_08a);
-  t.execute("08b", solve_08b);
+  // t.execute("08a", solve_08a);
+  // t.execute("08b", solve_08b);
 
-  t.execute("09a", solve_09a);
-  t.execute("09b", solve_09b);
+  // t.execute("09a", solve_09a);
+  // t.execute("09b", solve_09b);
 
-  t.execute("10a", solve_10a);
-  t.execute("10b", solve_10b);
+  // t.execute("10a", solve_10a);
+  // t.execute("10b", solve_10b);
 
-  t.execute("11a", solve_11a);
-  t.execute("11b", solve_11b);
+  // t.execute("11a", solve_11a);
+  // t.execute("11b", solve_11b);
 
-  t.execute("12a", solve_12a);
-  t.execute("12b", solve_12b);
+  // t.execute("12a", solve_12a);
+  // t.execute("12b", solve_12b);
 
-  t.execute("13a", solve_13a);
-  t.execute("13b", solve_13b);
+  // t.execute("13a", solve_13a);
+  // t.execute("13b", solve_13b);
 
-  t.execute("14a", solve_14a);
-  t.execute("14b", solve_14b);
+  // t.execute("14a", solve_14a);
+  // t.execute("14b", solve_14b);
 
-  t.execute("15a", solve_15a);
-  t.execute("15b", solve_15b);
+  // t.execute("15a", solve_15a);
+  // t.execute("15b", solve_15b);
 
-  t.execute("16a", solve_16a);
-  t.execute("16b", solve_16b);
+  // t.execute("16a", solve_16a);
+  // t.execute("16b", solve_16b);
 
-  t.execute("17a", solve_17a);
-  t.execute("17b", solve_17b);
+  // t.execute("17a", solve_17a);
+  // t.execute("17b", solve_17b);
+
+  t.execute("18a", solve_18a);
+  t.execute("18b", solve_18b);
 }
 
 void year2020() {
