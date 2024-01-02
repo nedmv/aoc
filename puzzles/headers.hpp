dc2cd779beacea3bc9cf99b9e5261e4ea2895d27
nedmv <nedmvl@rambler.ru>
2024-01-02 16:26:17 +0300

    feat: add file with default headers

---
+++ puzzles/headers.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <thread>
+
+// Containers
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+// Container adaptors
+#include <deque>
+#include <queue>
+#include <stack>
